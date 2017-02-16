482d76ed1361c528eb63bd500af37be5c0621b7a
Yuri <yuri.manahan@gmail.com>
2017-02-15 21:40:35 -0500

    Fixed bugs

--- js/main.js
+++ js/main.js
@@ -13,19 +13,67 @@ $(function(){
       }
     turn++;
     }
-    over();
+    horizontal();
   });
 
 
-  function over(){
-    if (turn >5){
-    for (i = 0; i <= 2; i++){
-      if ($(".square:has(.circle)")[i] == $(".square:has(.circle)")){
+  function horizontal(){
+    if (turn >=5){
 
-      alert("Circle won!");
+      if ($($('.square')[0]).children().hasClass(move.className) &&
+      $($('.square')[1]).children().hasClass(move.className) &&
+      $($('.square')[2]).children().hasClass(move.className)){
+      alert( move.className + " won!");
       }
+
+      if ($($('.square')[3]).children().hasClass(move.className) &&
+      $($('.square')[4]).children().hasClass(move.className) &&
+      $($('.square')[5]).children().hasClass(move.className)){
+      alert( move.className + " won!");
+      }
+
+      if ($($('.square')[6]).children().hasClass(move.className) &&
+      $($('.square')[7]).children().hasClass(move.className) &&
+      $($('.square')[8]).children().hasClass(move.className)){
+      alert( move.className + " won!");
+      }
+      vertical();
     }
   }
-}
+
+  function vertical(){
+      if ($($('.square')[0]).children().hasClass(move.className) &&
+          $($('.square')[3]).children().hasClass(move.className) &&
+          $($('.square')[6]).children().hasClass(move.className)){
+          alert( move.className + " won!");
+      }
+
+      else if ($($('.square')[1]).children().hasClass(move.className) &&
+        $($('.square')[4]).children().hasClass(move.className) &&
+        $($('.square')[7]).children().hasClass(move.className)){
+        alert( move.className + " won!");
+      }
+
+      else if ($($('.square')[2]).children().hasClass(move.className) &&
+        $($('.square')[5]).children().hasClass(move.className) &&
+        $($('.square')[8]).children().hasClass(move.className)){
+        alert( move.className + " won!");
+      }
+    diagonal();
+  }
+
+  function diagonal(){
+      if ($($('.square')[0]).children().hasClass(move.className) &&
+          $($('.square')[4]).children().hasClass(move.className) &&
+          $($('.square')[8]).children().hasClass(move.className)){
+          alert( move.className + " won!");
+      }
+
+      else if ($($('.square')[2]).children().hasClass(move.className) &&
+        $($('.square')[4]).children().hasClass(move.className) &&
+        $($('.square')[6]).children().hasClass(move.className)){
+        alert( move.className + " won!");
+      }
+  }
 
 })
