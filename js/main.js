937447bd9e15b37aa0b52f0a1243fd894d0156d8
Yuri <yuri.manahan@gmail.com>
2017-02-16 10:22:57 -0500

    Refactored code

--- js/main.js
+++ js/main.js
@@ -14,81 +14,89 @@ $(function(){
       }
     turn++;
     }
-    horizontal();
+    winnerChecker();
   });
 
 
 
-  function horizontal(){
+  function winnerChecker(){
     if (turn >=5){
+    var winners = [[0,1,2], [3,4,5], [6,7,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+      for (i = 0 ; i <=8; i++){
+        var b = winners[i][0];
+        var c = winners[i][1];
+        var d = winners[i][2];
 
-      if ($($('.square')[0]).children().hasClass(move.className) &&
-      $($('.square')[1]).children().hasClass(move.className) &&
-      $($('.square')[2]).children().hasClass(move.className)){
-      alert( move.className + " won!");
-      $('.square').empty();
+          if ($($('.square')[b]).children().hasClass(move.className) &&
+          $($('.square')[c]).children().hasClass(move.className) &&
+          $($('.square')[d]).children().hasClass(move.className)){
+            alert( move.className + " won!");
+            $('.square').empty();
+          }
+        }
       }
-
-      if ($($('.square')[3]).children().hasClass(move.className) &&
-      $($('.square')[4]).children().hasClass(move.className) &&
-      $($('.square')[5]).children().hasClass(move.className)){
-      alert( move.className + " won!");
-      $('.square').empty();
-      }
-
-      if ($($('.square')[6]).children().hasClass(move.className) &&
-      $($('.square')[7]).children().hasClass(move.className) &&
-      $($('.square')[8]).children().hasClass(move.className)){
-      alert( move.className + " won!");
-      $('.square').empty();
-      }
-      vertical();
-    }
-  }
-
-  function vertical(){
-      if ($($('.square')[0]).children().hasClass(move.className) &&
-          $($('.square')[3]).children().hasClass(move.className) &&
-          $($('.square')[6]).children().hasClass(move.className)){
-          alert( move.className + " won!");
-          $('.square').empty();
-      }
-
-      else if ($($('.square')[1]).children().hasClass(move.className) &&
-        $($('.square')[4]).children().hasClass(move.className) &&
-        $($('.square')[7]).children().hasClass(move.className)){
-        alert( move.className + " won!");
-        $('.square').empty();
-      }
-
-      else if ($($('.square')[2]).children().hasClass(move.className) &&
-        $($('.square')[5]).children().hasClass(move.className) &&
-        $($('.square')[8]).children().hasClass(move.className)){
-        alert( move.className + " won!");
-        $('.square').empty();
-      }
-    diagonal();
-  }
-
-  function diagonal(){
-      if ($($('.square')[0]).children().hasClass(move.className) &&
-          $($('.square')[4]).children().hasClass(move.className) &&
-          $($('.square')[8]).children().hasClass(move.className)){
-          alert( move.className + " won!");
-          $('.square').empty();
-      }
-
-      else if ($($('.square')[2]).children().hasClass(move.className) &&
-        $($('.square')[4]).children().hasClass(move.className) &&
-        $($('.square')[6]).children().hasClass(move.className)){
-        alert( move.className + " won!");
-        $('.square').empty();
-      }
-
-      else if (turn == 9){
-        alert("Draw...");
-        $('.square').empty();
-      }
-  }
+  };
+  //
+  //     if ($($('.square')[3]).children().hasClass(move.className) &&
+  //     $($('.square')[4]).children().hasClass(move.className) &&
+  //     $($('.square')[5]).children().hasClass(move.className)){
+  //     alert( move.className + " won!");
+  //     $('.square').empty();
+  //     }
+  //
+  //     if ($($('.square')[6]).children().hasClass(move.className) &&
+  //     $($('.square')[7]).children().hasClass(move.className) &&
+  //     $($('.square')[8]).children().hasClass(move.className)){
+  //     alert( move.className + " won!");
+  //     $('.square').empty();
+  //     }
+  //     vertical();
+  //   }
+  // }
+  //
+  // function vertical(){
+  //     if ($($('.square')[0]).children().hasClass(move.className) &&
+  //         $($('.square')[3]).children().hasClass(move.className) &&
+  //         $($('.square')[6]).children().hasClass(move.className)){
+  //         alert( move.className + " won!");
+  //         $('.square').empty();
+  //     }
+  //
+  //     else if ($($('.square')[1]).children().hasClass(move.className) &&
+  //       $($('.square')[4]).children().hasClass(move.className) &&
+  //       $($('.square')[7]).children().hasClass(move.className)){
+  //       alert( move.className + " won!");
+  //       $('.square').empty();
+  //     }
+  //
+  //     else if ($($('.square')[2]).children().hasClass(move.className) &&
+  //       $($('.square')[5]).children().hasClass(move.className) &&
+  //       $($('.square')[8]).children().hasClass(move.className)){
+  //       alert( move.className + " won!");
+  //       $('.square').empty();
+  //     }
+  //   diagonal();
+  // }
+  //
+  // function diagonal(){
+  //     if ($($('.square')[0]).children().hasClass(move.className) &&
+  //         $($('.square')[4]).children().hasClass(move.className) &&
+  //         $($('.square')[8]).children().hasClass(move.className)){
+  //         alert( move.className + " won!");
+  //         $('.square').empty();
+  //     }
+  //
+  //     else if ($($('.square')[2]).children().hasClass(move.className) &&
+  //       $($('.square')[4]).children().hasClass(move.className) &&
+  //       $($('.square')[6]).children().hasClass(move.className)){
+  //       alert( move.className + " won!");
+  //       $('.square').empty();
+  //     }
+  //
+  //     else if (turn == 9){
+  //       alert("Draw...");
+  //       $('.square').empty();
+  //     }
+  // }
 
 })
